db1827d3de265b10c9a099caae72fc1c539b04e2
MohamedHassan499 <MohamedHassan499@protonmail.com>
2024-04-16 02:04:20 +0200

    finish seeding script

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,6 +1,7 @@
-import { faker } from '@faker-js/faker'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import ExcelJs from 'exceljs'
+import { faker } from '@faker-js/faker'
 import { Brand } from '../db/brands-schema'
 dotenv.config()
 
@@ -21,5 +22,18 @@ var currentYear = new Date().getFullYear()
     brands.push(brand)
   }
   await Brand.insertMany(brands)
-  
+  const workbook = new ExcelJs.Workbook()
+
+  // Add a worksheet
+  const worksheet = workbook.addWorksheet('Brands')
+  worksheet.columns = [
+    { header: 'ID', key: 'id', width: 50 },
+    { header: 'Brand Name', key: 'brandName', width: 32 },
+    { header: 'Year Founded', key: 'yearFounded', width: 15 },
+    { header: 'Headquarters', key: 'headquarters', width: 20 },
+    { header: 'Number of Locations', key: 'numberOfLocations', width: 20 },
+  ]
+  worksheet.addRows(brands)
+  await workbook.xlsx.writeFile('output/brands.xlsx')
+  console.log('Brands exported to Excel')
 })()
