5b566f65fd7bb2ed530ceedba5ab950f81526543
MohamedHassan499 <MohamedHassan499@protonmail.com>
2024-04-16 02:11:47 +0200

    fix: validate numeric fields

--- scripts/import.ts
+++ scripts/import.ts
@@ -15,6 +15,12 @@ type BrandType = {
 const MIN_YEAR = 1600
 const MIN_LOCATIONS = 1
 
+const getNumericValues = (brand: Record<string, any>) => {
+  return Object.values(brand)
+    .filter((value) => typeof value === 'number')
+    .sort((a, b) => +a - +b)
+}
+
 const validYearFounded = (yearFounded: number) => {
   if (typeof yearFounded !== 'number') {
     return false
@@ -36,6 +42,7 @@ const validNumberOfLocations = (numberOfLocations: number) => {
     await mongoose.connect(process.env.DATABASE_URL!)
     const validBrands: BrandType[] = []
     brands.forEach(async (brand) => {
+      const numericValues: number[] = getNumericValues(brand)
       if (!brand.brandName || !brand.headquarters) {
         return
       }
@@ -43,11 +50,16 @@ const validNumberOfLocations = (numberOfLocations: number) => {
       brand.headquarters = brand.headquarters.toString().trim()
       if (brand.hasOwnProperty('yearFounded') && !validYearFounded(brand.yearFounded as number)) {
         brand.yearFounded = MIN_YEAR
+      } else if (!brand.hasOwnProperty('yearFounded')) {
+        brand.yearFounded = numericValues[0]
+        numericValues.unshift()
       }
       if (brand.hasOwnProperty('numberOfLocations') && !validNumberOfLocations(brand.numberOfLocations as number)) {
         brand.numberOfLocations = MIN_LOCATIONS
+      } else if (!brand.hasOwnProperty('numberOfLocations')) {
+        brand.numberOfLocations = numericValues[0]
+        numericValues.unshift()
       }
-
       if (
         validYearFounded(brand.yearFounded as number) &&
         validName(brand.brandName as string) &&
